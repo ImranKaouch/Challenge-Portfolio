61f78d7843c1ef99c895975bbc387666f2f1acfe
ImranKaouch <2122928@talnet.nl>
2021-03-05 11:00:52 +0100

    Portfolio

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,3 +1,15 @@
-bezoeker {
-    
-}
+let bezoeker= {
+    voornaam: "Welkom "+ prompt("Wat is uw voornaam?"),
+    leeftijd: prompt("Wat is uw leeftijd?")
+};
+console.log(bezoeker);
+
+document.getElementById("voornaam").innerHTML= bezoeker.voornaam;
+
+if (bezoeker.leeftijd > 18){
+
+    document.querySelector("h1"). style.color="green"
+    }
+    else if (bezoeker.leeftijd < 18) {
+    document.querySelector("h1"). style.color="red"
+    }
